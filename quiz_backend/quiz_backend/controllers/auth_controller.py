081a88f4334d613e340d8a1f7524915288984c53
afzalfaizi <afzaalm993@gmail.com>
2024-09-10 10:27:54 +0500

    authentication (JWT Access Token)

--- quiz_backend/quiz_backend/controllers/auth_controller.py
+++ quiz_backend/quiz_backend/controllers/auth_controller.py
@@ -8,28 +8,23 @@ from datetime import timedelta
 from quiz_backend.settings import algorithm
 # Create an instance of CryptContext, using bcrypt for password hashing.
 pwd_context = CryptContext(schemes="bcrypt")
+
+
+
 # Function to generate an access token
 # Parameters:
 #   - data (dict): the payload to be encoded in the token
 #   - expiry_time (timedelta): the token's expiration time
-def generateAccessToken(data, expiry_time: timedelta):
+def generateToken(data, expiry_time: timedelta):
     try:
-        # Make a copy of the payload data
         to_encode_data = data.copy() 
-        # Add expiration time to the token payload
         to_encode_data.update({
             "exp": expiry_time
         })
-        # Encode the data into a JWT using the provided secret key and algorithm
-        access_token = jwt.encode(to_encode_data, secret_key, algorithm="algorithm")
-        # Return the generated access token
-        return access_token
+        token = jwt.encode(to_encode_data, secret_key, algorithm="algorithm")
+        return token
     except JWTError as je:
         print(je)
-
-# Placeholder function to generate a refresh token
-def generateRefreshToken():
-    ...
 
 # Placeholder function to verify passwords
 def verfiyPassword():
