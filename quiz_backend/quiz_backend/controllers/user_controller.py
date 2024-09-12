9c07486041fad827f8373d99583cdaf89814e5d9
afzalfaizi <afzaalm993@gmail.com>
2024-09-12 16:55:41 +0500

    Implement user sign-up with email verification, password hashing, and session management

--- quiz_backend/quiz_backend/controllers/user_controller.py
+++ quiz_backend/quiz_backend/controllers/user_controller.py
@@ -1,9 +1,17 @@
 from quiz_backend.utils.imports import User, Token, UserModel, Session, select
+from quiz_backend.utils.exception import ConflictException
 
 
 
 def signUp(user_form:UserModel, session: Session):
     # TODO: verify if user exist
-    # select(User).where(User.user_email==user_form.user_email)
+    user_exist = session.exec(select(User).where(User.user_email==user_form.user_email)).all()
+    if user_exist:
+        raise ConflictException("email")
     hashed_password = passwordIntoHash(user_form.user_password)
-    user(user_name= user_form.user_name,user_email=user_form.user_email,)
+    user(user_name= user_form.user_name,user_email=user_form.user_email,user_password=hashed_password)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    #TODO: generate access token and refresh token
